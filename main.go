11203a062b97495e1cf213abb873d40d35819844
Warren Wu <76506508+VVarren@users.noreply.github.com>
2022-04-06 14:38:19 -0400

    PROBLEM FIXED
    Ok so the problem was that i had a random print statement at the start of my code so it always said computer won. I also found a way to use a global variable. pretty cool

--- main.go
+++ main.go
@@ -6,26 +6,29 @@ import (
 	"time"
 )
 
+var arr [14]int = [14]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11}
+
 //TO-DO LISt
 // UI
 // Maybe make the computer more intelligent and entirely up to the player
 
 func main() {
 	var input string
-	arr := [14]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11}
+
 	notWin := true
 	playerHand := initialHand()
 	dealerHand := initialHand()
 	rand.Seed(time.Now().UnixNano())
 
 	for notWin {
-		fmt.Print(playerHand)
-		fmt.Println("Player Hand")
+		fmt.Println(playerHand, " Player Hand")
+		fmt.Println(dealerHand, " Computer Hand")
+
 		fmt.Println("Type 'Draw' to draw a card and 'Quit' to quit")
 		fmt.Println("If you wnat to change your ace(11) to a 1 or vice versa type 'Change' ")
 
 		fmt.Scan(&input)
-		fmt.Println("Computer Won")
+
 		if input == "Quit" {
 
 			if winTeller(playerHand, dealerHand, 2) == "Player has won!" {
@@ -40,17 +43,17 @@ func main() {
 		} else if input == "Change" {
 			playerHand = oneToEleven(playerHand)
 			fmt.Print("It has been changed")
-			fmt.Println(playerHand)
+			fmt.Println(playerHand, " -> Player Hand")
 
 		} else if input == "Draw" {
 			playerHand = append(playerHand, arr[rand.Intn(len(arr)-1)])
 			dealerHand = append(dealerHand, arr[rand.Intn(len(arr)-1)])
-			fmt.Print(playerHand)
-			fmt.Println(dealerHand)
-			if winTeller(playerHand, dealerHand, 1) == "Computer Lost" {
+			fmt.Print(playerHand, " -> Player Hand")
+			fmt.Println(dealerHand, "-> Dealer Hand")
+			if winTeller(playerHand, dealerHand, 3) == "Computer Lost" {
 				fmt.Println("Player has won")
 				notWin = false
-			} else if winTeller(playerHand, dealerHand, 1) == "Player Lost" {
+			} else if winTeller(playerHand, dealerHand, 3) == "Player Lost" {
 				fmt.Println("Computer Won")
 				notWin = false
 			}
@@ -76,7 +79,6 @@ func initialHand() []int {
 	r1, r2 := arr[rand.Intn(len(arr)-1)], arr[rand.Intn(len(arr)-1)]
 	list = append(list, r1)
 	list = append(list, r2)
-	fmt.Println(list)
 	return list
 }
 
@@ -92,6 +94,16 @@ func winTeller(playerHand []int, dealerHand []int, cases int) string {
 
 	case 2:
 		if sum(playerHand) > sum(dealerHand) {
+			return "Player has won!"
+		} else if sum(dealerHand) > sum(playerHand) {
+			return "Computer has won!"
+		}
+	case 3:
+		if sum(dealerHand) > 21 {
+			return "Computer Lost"
+		} else if sum(playerHand) > 21 {
+			return "Player Lost"
+		} else if sum(playerHand) > sum(dealerHand) {
 			return "Player has won!"
 		} else if sum(dealerHand) > sum(playerHand) {
 			return "Computer has won!"
